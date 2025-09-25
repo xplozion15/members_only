23a91054e123fc62941174f72b13adc6d6cb29ae
xplozion15 <89824888+xplozion15@users.noreply.github.com>
2025-09-25 17:15:09 +0530

    feat: add an option in signup form to make admin accounts with a secret password

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,9 +1,8 @@
 const pool = require("../db/pool");
 const bcrypt = require("bcryptjs");
 const dotenv = require("dotenv");
-const db = require("../db/queries")
+const db = require("../db/queries");
 dotenv.config();
-
 
 function showIndexPage(req, res) {
   res.render("indexPage");
@@ -13,10 +12,9 @@ function showSignupPage(req, res) {
   res.render("signUpPage", {});
 }
 
-function showMembershipForm(req,res) {
-  res.render("membershipForm",{});
+function showMembershipForm(req, res) {
+  res.render("membershipForm", {});
 }
-
 
 function showLoginPage(req, res) {
   if (req.user) {
@@ -27,30 +25,56 @@ function showLoginPage(req, res) {
 }
 
 async function postUserToDb(req, res, next) {
-  
+
+  const isUserAdmin = req.body["is_admin"]; // on or off expected value from the req body through the form
+
+  const userInputAdminPassword = req.body["admin-password"];
+  const adminPassword = process.env.ADMIN_PASSWORD;
+  const doesAdminPasswordMatch = userInputAdminPassword === adminPassword;
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    await pool.query(
-      "INSERT INTO users (username,email,password) VALUES ($1,$2,$3)",
-      [req.body.username, req.body.email, hashedPassword],
-    );
-    res.redirect("/");
+
+    if (isUserAdmin === undefined) {
+  
+        await pool.query(
+          "INSERT INTO users (username,email,password) VALUES ($1,$2,$3)",
+          [req.body.username, req.body.email, hashedPassword],
+        );
+        res.redirect("/");
+    }
+    else if (isUserAdmin === "on") {
+      if (doesAdminPasswordMatch) {
+        await pool.query(
+          "INSERT INTO users (username,email,password,is_admin) VALUES ($1,$2,$3,$4)",
+          [req.body.username, req.body.email, hashedPassword,"TRUE"],
+        );
+        res.redirect("/");
+      }
+      else {
+        res.send("wrong password bruh?")
+      }
+    }
   } catch (error) {
     return next(error);
   }
 }
 
-async function giveMembership(req,res) {
-    const userId = req.user.id;
-    const userPasswordInput = req.body["membership-password"];
-    if(userPasswordInput === process.env.MEMBERSHIP_PASSWORD) {
-      await db.markMembershipInDb(userId);
-      res.redirect("/")
-    }
-    else {
-      res.redirect("/membership-form")
-    }
+async function giveMembership(req, res) {
+  const userId = req.user.id;
+  const userPasswordInput = req.body["membership-password"];
+  if (userPasswordInput === process.env.MEMBERSHIP_PASSWORD) {
+    await db.markMembershipInDb(userId);
+    res.redirect("/");
+  } else {
+    res.redirect("/membership-form");
+  }
 }
 
-
-module.exports = { showIndexPage, showSignupPage, postUserToDb, showLoginPage ,showMembershipForm,giveMembership};
+module.exports = {
+  showIndexPage,
+  showSignupPage,
+  postUserToDb,
+  showLoginPage,
+  showMembershipForm,
+  giveMembership,
+};
